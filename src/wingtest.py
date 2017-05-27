0b285af02e9b601ed2b5ef3bc45ce94a592419ad
justttry <kerzhao@163.com>
2017-05-27 11:25:20 +0800

    update

--- src/wingtest.py
+++ src/wingtest.py
@@ -1,6 +1,11 @@
 #encoding:UTF-8
 
 import wingdbstub
+import thread
+wingdbstub.Ensure(require_connection=1, require_debugger=1)
+wingdbstub.kEmbedded = 1
+thread_id = thread.get_ident()
+wingdbstub.debugger.SetDebugThreads({thread_id:1})
 
 print 'haha'
 
