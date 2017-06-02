d8bc8236fe4cec60c2b87f5d2bdd64022d5aa934
kerzhao <kerzhao@163.com>
2017-06-02 09:06:37 +0000

    update

--- src/wingtest.py
+++ src/wingtest.py
@@ -4,6 +4,7 @@ import wingdbstub
 import thread
 wingdbstub.Ensure(require_connection=1, require_debugger=1)
 wingdbstub.kEmbedded = 1
+wingdbstub.kExitOnFailure = 1
 thread_id = thread.get_ident()
 wingdbstub.debugger.SetDebugThreads({thread_id:1})
 
@@ -13,3 +14,11 @@ def f():
     return 'hello world'
 
 print f()
+a = 0
+for i in xrange(1000):
+    a += 1
+print "Entered the debugger"
+
+raw_input("Hit Enter to proceed")
+
+print "\nAnother statement"
